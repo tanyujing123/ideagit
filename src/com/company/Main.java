192aa5a7d6ea182ec24ff4dad22eea68580c1f6e
tanyuijng123 <570386159@qq.com>
2019-09-17 23:26:50 +0800

    4修改

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello Git,I");
-        System.out.println("hello Git,I");
-        System.out.println("hello Git,I");
+
+
         System.out.println("hello Git,I");
 
         System.out.println("hello Git,1113311111");
-       
-        System.out.println("3222");
+
+        int d=222;
+        int da=2234;
+
+
+
     }
 }
