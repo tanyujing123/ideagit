594ccb64efa8d0e7bf6ec406dccf3573a345d91c
tanyuijng123 <570386159@qq.com>
2019-09-17 23:00:00 +0800

    second 修改

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello Git,I");
-        System.out.println("2222222222");
+        System.out.println("111111");
+        System.out.println("444444");
     }
 }
